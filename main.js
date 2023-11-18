865875ada44c92d8d8dab252175f49ec89937423
elokthewizard <148482234+elokthewizard@users.noreply.github.com>
2023-11-18 12:23:46 -0700

    write createGrid funciton and replace html container with dynamic js

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-const container = document.querySelector("#container")
+function createGrid() {
+    const container = document.createElement('div');
+    container.id = 'main';
+    container.className = 'container';
+
+    for (let i = 0; i < 16; i++) {
+        const row = container.appendChild(document.createElement('div'));
+        row.className = 'row';
+        row.id = `row_${i}`
+        
+        for (let k = 0; k < 16; k++){
+            const box = row.appendChild(document.createElement('div'));
+            box.className = 'box';
+        };
+            
+    };
+
+    document.body.appendChild(container);
+};
+
+createGrid();
